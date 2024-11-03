b24bbddf83074eea704cdba685f85e174bf77f7a
Daniel Gustaw <gustaw.daniel@gmail.com>
2024-11-03 14:20:30 +0400

    rust matrix multiplication

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use burn::tensor::{Tensor, backend::Backend};
+fn computation<B: Backend>() {
+    // Create the device where to do the computation
+    let device = Default::default();
+
+    let tensor1: Tensor<B, 2> = Tensor::from_floats([[2., 3.], [4., 5.]], &device);
+    let tensor2 = Tensor::ones_like(&tensor1);
+
+    println!("{:}", tensor1.matmul(tensor2));
+}
+
 fn main() {
-    println!("Hello, world!");
+    computation::<burn::backend::Wgpu>();
 }
